9f4b94c79eef45306189f6c823f82d401cb4ba3e
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2024-10-08 20:26:39 +0300

    Add new program

---
+++ main.py
@@ -0,0 +1,78 @@
+import file_operations
+from faker import Faker
+import random
+import os
+
+QUANTITY_OF_CARDS = 10
+
+
+def main():
+    if not os.path.exists('generated_cards'):
+        os.makedirs('generated_cards')
+    fake_data = Faker('ru_RU')
+
+    alphabet = {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' ',
+    }
+
+    skills = [
+        'Стремительный прыжок',
+        'Электрический выстрел',
+        'Ледяной удар',
+        'Стремительный удар',
+        'Кислотный взгляд',
+        'Тайный побег',
+        'Ледяной выстрел',
+        'Огненный заряд',
+    ]
+
+    runic_skills = []
+    for skill in skills:
+        for letter in skill:
+            skill = skill.replace(letter, alphabet[letter])
+        runic_skills.append(skill)
+
+    for card in range(QUANTITY_OF_CARDS):
+        chosen_skills = random.sample(runic_skills, 3)
+        names = {
+            'first_name': fake_data.first_name(),
+            'last_name': fake_data.last_name(),
+            'job': fake_data.job(),
+            'town': fake_data.city(),
+            'strength': random.randint(3, 18),
+            'agility': random.randint(3, 18),
+            'endurance': random.randint(3, 18),
+            'intelligence': random.randint(3, 18),
+            'luck': random.randint(3, 18),
+            'skill_1': chosen_skills[0],
+            'skill_2': chosen_skills[1],
+            'skill_3': chosen_skills[2],
+        }
+        file_operations.render_template('template_of_card\\charsheet.svg',
+                                        'generated_cards\\card_{0}.svg'.format(card), names)
+
+
+if __name__ == '__main__':
+    main()
